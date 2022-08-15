7b30202c09c61e424d7c3c8d7ec22304c2a76a94
SHARAN KUMAR <45118936+SharanKumarCode@users.noreply.github.com>
2022-08-15 15:25:08 +0530

    added products data interface

---
+++ src/app/products/interfaces/productdata.interface.ts
@@ -0,0 +1,12 @@
+export interface IProductData {
+    productId: string;
+    productName: string;
+    group: string;
+    description: string;
+    stock: number;
+    priceDirectSale: number;
+    priceReseller: number;
+    priceDealer: number;
+    createdDate: Date;
+    sold: number;
+  }
