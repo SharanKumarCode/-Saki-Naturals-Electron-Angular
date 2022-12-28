87b631b335eec214c8e4e1b00d87ee43d9e11a04
SHARAN KUMAR <45118936+SharanKumarCode@users.noreply.github.com>
2022-12-28 16:50:43 +0530

    implemented global notification service - snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { APP_CONFIG } from '../environments/environment';
+import { Subject } from 'rxjs';
+import { NotificationService } from './core/services/notification/notification.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +13,29 @@ import { APP_CONFIG } from '../environments/environment';
 })
 export class AppComponent {
   timerFlag = true;
+  private snackBarMessageObservable: Subject<string>;
 
   constructor(
     private electronService: ElectronService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private notificationService: NotificationService,
+    private snackbar: MatSnackBar,
   ) {
     this.translate.setDefaultLang('en');
     console.log('APP_CONFIG', APP_CONFIG);
 
-    if (electronService.isElectron) {
+    if (this.electronService.isElectron) {
       console.log(process.env);
       console.log('Run in electron');
-      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
-      console.log('NodeJS childProcess', this.electronService.childProcess);
     } else {
       console.log('Run in browser');
     }
 
     setInterval(()=>this.timerFlag = false, 2500);
+
+    this.snackBarMessageObservable = this.notificationService.getSnackBarMessageSubject();
+    this.snackBarMessageObservable.subscribe(message=>{
+      this.snackbar.open(message, 'close');
+    });
   }
 }
